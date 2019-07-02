21db3a8a27b440bdb07528781fe16191087b1040
Melanie <m.maddix@gmail.com>
2019-07-02 23:07:42 +0000

    adds chatbar input events

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -3,18 +3,33 @@ import React, { Component } from "react";
 export default class ChatBar extends Component {
   constructor() {
     super();
-    // this.state = {};
+    this.checkEnter = this.checkEnter.bind(this);
+  }
+
+  checkEnter(event) {
+    if(event.key === "Enter") {
+      const msg = {
+        user: this.refs.form.chatuser.value,
+        content: this.refs.form.chatmessage.value
+      };
+      console.log(msg);
+    }
   }
 
   render() {
 
     return(
         <footer className="chatbar">
-          <input className="chatbar-username"
-            placeholder="Your Name (Optional)"
-            defaultValue={this.props.currentUser.name}
-          />
-          <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+          <form ref="form">
+            <input name="chatuser" className="chatbar-username"
+              placeholder="Your Name (Optional)"
+              defaultValue={this.props.currentUser.name}
+            />
+            <input name="chatmessage" className="chatbar-message"
+              placeholder="Type a message and hit ENTER"
+              onKeyDown={this.checkEnter}
+            />
+          </form>
         </footer>
     );
   }
