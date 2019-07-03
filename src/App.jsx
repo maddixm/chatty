17fa4406adb7720c615ac1f983d40fac4afe5ff6
Melanie <m.maddix@gmail.com>
2019-07-03 17:48:43 +0000

    removes fake messages from constructor

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
-      messages:[
-                {
-                  id: 1,
-                  username: "Bob",
-                  content: "Has anyone seen my marbles?",
-                },
-                {
-                  id: 2,
-                  username: "Anonymous",
-                  content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-                }
-              ]
+      currentUser: {name: "Bob"}, // If currentUser is not defined, will be Anonymous
+      messages:[] //messages from server will be stored here as they arrive
     };
 
   }
